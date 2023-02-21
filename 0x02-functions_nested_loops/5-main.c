d16bb47951111fa70eac9747f207ed86bef6990d
Abdulkareem Abdurrazzaq <abdulkareemabdurrazzaq726@gmail.com>
2023-02-21 13:15:54 -0800

    print last digit 7

---
+++ 0x02-functions_nested_loops/5-main.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0 (success)
+ */
+int print_sign(int n)
+{
+	int r;
+
+	r = print_sign(void);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(void);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(void);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	r = print_sign(void);
+	_putchar(',');
+	_putchar(' ');
+	_putchar(r + '0');
+	_putchar('\n');
+	return (0);
+}
